3f72d52d073bda5e8426c07a5f36d002d405394a
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-02-06 12:00:35 +0000

    added init_dep file: Another way of depinject

---
+++ lib/controller/init_dependency.dart
@@ -0,0 +1,16 @@
+// import 'package:get/get.dart';
+
+// import 'package:get/get.dart';
+// import 'package:getx/controller/list_controller.dart';
+// import 'package:getx/controller/tap_controller.dart';
+
+// class InitDep implements Bindings{
+//   @override
+//   void dependencies(){
+//     Get.lazyPut(() => TapController());
+//     Get.lazyPut(() => ListController());
+
+//   }
+//    // Your main class will be a future Aand an async class 
+//   // add InitialBinding to Main.dart file under your materialApp or GetMaterialApp 
+// }
